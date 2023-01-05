4da3dc8c6d05d4e9b3fb953c304f7220c3bc2bd9
MrTESSARD <dimitri@tessard.pro>
2023-01-05 13:33:13 +0100

    Add Header, Landing and Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ const Footer = () => {
+  return (
+    <footer>
+        <div className='footer-container'>
+            <p>Projet réalisé par Uncia</p>
+            <p>Les icônes Wolvering, Iron-man, Spider-man, Batman sont prises sur iconeFinder.com </p>
+        </div>
+    </footer>
+  )
+}
+export default  Footer 
